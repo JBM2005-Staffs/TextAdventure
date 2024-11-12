93d8d98c8312315c66473365a5d5f01233effff9
JBM2005-Staffs <m018517o@student.staffs.ac.uk>
2024-11-12 00:39:49 +0000

    Add inital files via upload
    Includes Main.cpp and Player.h

---
+++ TextAdventure/Player.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#pragma once
+
+
+
+class Player
+{
+public:
+    string name;
+    float health = 100.0f;
+    int score = 0;
+    //main weapon
+    //money
+    //
+
+};
+
---
+++ TextAdventure/TextAdventure.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include "Player.h"
+using namespace std;
+
+string input;
+
+int main()
+{
+    Player player;
+    cout << "Hello Adventurer, what is your name? \n" << "> ";
+    cin >> player.name;
+
+    //while player.name contains numbers then repeat the question, also character limit of 32
+    while ()
+    {
+        
+    }
+
+    cout << "So be it.\n";
+    // ask about a weapon to begin with
+    cout << "It is time,\nAre you ready " << player.name << "?\n";
+
+    cin >> input;
+   /* while (input)
+    {
+        //While input != yes or no, fail it and come back and ask again
+    }
+    return 0;*/
+
+
+}
+
+
+
+
+
